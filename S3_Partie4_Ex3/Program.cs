f82de505f95c66eafed734b6e6e58169ec4778f7
InasseY <inasse.youssouf@gmail.com>
2021-02-19 10:15:02 +0100

    boucle for with list

---
+++ S3_Partie4_Ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S3_Partie4_Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            List<int> number = new List<int>() { 10, 12, 19, 25, 28};
+
+            for (int i = 0; i < number.Count; i++)
+            {
+                result += number[i];
+                
+            }
+
+            Console.WriteLine($"le résultat de ton addition est de {result}");
+            
+
+        }
+    }
+}
